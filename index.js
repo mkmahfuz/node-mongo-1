1d9e835ef570ddb22cb25f10af52556fcde8e6b5
khalid <mdmahfuzbd@gmail.com>
2021-03-24 10:50:51 +0600

    mod 45-5 done

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send('Hello world')
+const fruits = { 'name': 'apple', 'price': 12 }
+app.get('/', (req, res) => {
+    res.send(fruits)
 })
 
-app.listen(port,()=>{
-    console.log('example app listening at',port)
+app.get('/banana/', (req, res) => {
+    res.send('hello banana')
+})
+
+const names = ['khalid', 'mahfuz', 'safia', 'sawda'];
+
+app.get('/users/:uid', (req, res) => {
+    console.log(req.params);
+    const userId = req.params.uid;
+    const name = names[userId];
+    res.send({userId,name});
+    console.log(req.query) //query from url ?sort=asc then at console {sort:'asc'}
+})
+
+app.listen(port, () => {
+    console.log('example app listening at', port)
 })
