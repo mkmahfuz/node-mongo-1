6049e1f8b65e90d9e78e507852fb02cb2e7b3362
khalid <mdmahfuzbd@gmail.com>
2021-03-24 15:52:02 +0600

    mod 45-9 done, Module-45 completed

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+// const bodyParser = require('body-parser'); // no need now
 const port = 3000;
+
+app.use(cors());
+// app.use(bodyParser.json()); // aita deprecated instead use app.use(express.json())
+app.use(express.json());
 
 const fruits = { 'name': 'apple', 'price': 12 }
 app.get('/', (req, res) => {
@@ -17,8 +23,15 @@ app.get('/users/:uid', (req, res) => {
     console.log(req.params);
     const userId = req.params.uid;
     const name = names[userId];
-    res.send({userId,name});
+    res.send({ userId, name });
     console.log(req.query) //query from url ?sort=asc then at console {sort:'asc'}
+})
+//post
+
+app.post('/adduser', (req, res) => {
+    // console.log(JSON.stringify(req.body));
+    console.log(req.body);
+    res.send(req.body)
 })
 
 app.listen(port, () => {
